c75acefe9cfba9019578c22ad3179d68305dc83d
Jesse Simpson <jesse.simpson36@gmail.com>
2019-03-14 20:28:46 -0400

    social network component by NokaK

---
+++ src/components/Social.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {  NavItem, NavLink } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+import { faInstagram } from "@fortawesome/free-brands-svg-icons"
+import { faPinterest } from "@fortawesome/free-brands-svg-icons"
+import './Social.css';
+
+class Social extends Component {
+	render() {
+		return (
+			<div>
+				<ul className="position-fixed social-ul">
+            <NavItem className="social-li">
+                <NavLink className="fb" href="https://www.facebook.com/">
+                <FontAwesomeIcon icon={faFacebook} />
+                </NavLink >
+            </NavItem>
+            <NavItem className="social-li">
+                <NavLink className="instagram" href="https://www.instagram.com/">
+                  <FontAwesomeIcon icon={faInstagram}/>
+                </NavLink >
+            </NavItem>
+            <NavItem className="social-li">
+                <NavLink  className="pin" href ="https://www.pinterest.com/">
+                  <FontAwesomeIcon icon={faPinterest} />
+                </NavLink >
+            </NavItem>
+        </ul>
+			</div>
+				);
+      }
+		}
+
+export default Social;
