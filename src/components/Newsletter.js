d708be27e52465bb2fdeee5286637db05f40da91
“Nokak” <nokakipiani93@gmail.com>
2019-03-22 02:03:26 +0400

    add online workshop component

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { InputGroup, InputGroupText, InputGroupAddon, Input } from 'reactstrap';
+import { InputGroup, Button, InputGroupAddon, Input } from 'reactstrap';
 
 import '../styles/Newsletter.css';
 
@@ -8,14 +8,14 @@ const Newsletter = (props) => {
     <div>
       <div className="container mx-auto w-50 my-5">
         <div className="wrapper">
-          <p className="text-center">text</p>
-          <h3  className="text-center my-3">text</h3>
-          <label> Email</label>          
+          <p className="text-center"> Join the LCWA newsletter!</p>
+          <h3  className="text-center my-3">Get weekly updates about events, courses online and discounts.</h3>
+          <label className="text-white mt-3">Email</label>          
           <InputGroup>
           <div className="d-flex justify-content-center">
             <Input />
             <InputGroupAddon addonType="append">
-              <InputGroupText>Join the newletter</InputGroupText>
+            <Button className="btn btn-primary my-2 my-sm-0">Join the newletter</Button>
             </InputGroupAddon>
             </div>
           </InputGroup>
