d82764a8e9bcac9b0060f76c820cb95c65453ebb
“Nokak” <nokakipiani93@gmail.com>
2019-04-14 15:02:22 +0400

    add newsletter responsive

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -6,16 +6,16 @@ import '../styles/Newsletter.css';
 const Newsletter = (props) => {
   return (
     <div>
-      <div className="container mx-auto w-50 my-5">
+      <div className="container mx-auto w-75 my-5">
         <div className="wrapper">
-          <p className="text-center"> Join the LCWA newsletter!</p>
-          <h3  className="text-center my-3">Get weekly updates about events, courses online and discounts.</h3>
+          <p className="text"> Join the LCWA newsletter!</p>
+          <h3  className="text my-3">Get weekly updates about events, courses online and discounts.</h3>
           <label className="text-white mt-3">Email</label>          
           <InputGroup>
-          <div className="d-flex justify-content-center">
+          <div className="input-content">
             <Input />
             <InputGroupAddon addonType="append">
-            <Button className="btn btn-primary my-2 my-sm-0">Join the newsletter</Button>
+            <Button className="btn btn-primary my-2 my-sm-0">Join newsletter</Button>
             </InputGroupAddon>
             </div>
           </InputGroup>
