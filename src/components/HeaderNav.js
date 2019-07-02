16a801bddc4bafea8961726a437942771c8fc51f
abburiswathi90 <abburiswathi90@gmail.com>
2019-07-02 22:23:25 +0530

    ID13 Change colors and fonts

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import LogoBlue from '../LogoTransparent.png';
+import MA_Logo from '../MA_Logo.png';
 import '../styles/Nav.css';
 
 class HeaderNav extends Component {
 	render() {
 		return (
 			<div>
-				<table width="78%">
-					<tr>
-						<td><img src={LogoBlue} alt="site logo" className = "nav-logo"></img> </td>
-                        <td>
-						<nav className="navbar navbar-expand-lg" role="navigation" >	
-							<Link className="nav-item nav-link" to="/">Home</Link>
-							<a className="nav-item nav-link" href="#AboutUs">About us</a>
-							<a className="nav-item nav-link" href="#ContactUs">Contact us</a>
-						</nav>
-				        </td>
-				     </tr>
-				</table>
+				<div className="d-flex justify-content-between  align-items-center px-3">
+				<img src={MA_Logo} alt="site logo" className = "nav-logo"></img>
+                    
+					<nav className="navbar navbar-expand-lg" role="navigation">	
+						<Link className="nav-item nav-link" to="/">Home</Link>
+						<a className="nav-item nav-link" href="#AboutUs">About us</a>
+						<a className="nav-item nav-link" href="#ContactUs">Contact us</a>
+					</nav>
+
+					<form className="form-inline my-2 my-lg-0">
+						<input className="form-control" type="search" placeholder="Search" aria-label="Search">
+						</input>	
+					</form>
+
+					<div className="burger">
+						<input className="menu-btn" type="checkbox" id="menu-btn" />
+						<label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
+						<ul className="menu">	
+						  	<li><Link to="/">Home</Link></li>
+             				<li><Link to="/Blog">Blog</Link></li>
+							<li><Link to="/Contact"></Link></li>
+						</ul>
+					</div>
+
 			 	</div>
 		
 				);
--- src/components/NewFooter.js
+++ src/components/NewFooter.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import '../styles/footer.css';
+import '../App.css';
 import { faLinkedinIn, faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Alex_Phone from '../Alex_Phone.JPG';
