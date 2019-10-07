33daa160f2946c6a91ccea71539192b8c39e3eb1
fadingbeat <fadingbeat@gmail.com>
2019-10-07 10:23:41 +0200

    Header component

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,66 +1,61 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import MA_Logo from '../images/MA_Logo.png';
-import '../styles/Nav.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import MA_Logo from "../images/LogoBlack_cut.png";
+import "../styles/Nav.css";
 
 class HeaderNav extends Component {
-	render() {
-		return (
-			 
-			<div className="d-flex justify-content-between  align-items-center px-3">
-				
-			<table width="90%"> 
-		    <tr> 
-			<td><img src={MA_Logo} alt="site logo" className = "nav-logo"></img></td>	
-              <div className="Home-bar">      
-			    <td align="right">	<nav className="navbar navbar-expand-lg" role="navigation" >	
-						<Link className="nav-item nav-link" to="/">Home</Link>
-						<a className="nav-item nav-link" href="#AboutUs">About us</a>
-						<a className="nav-item nav-link" href="#ContactUs">Contact us</a>
-					    </nav>				
-                </td>
-				</div>
-				</tr>
-				<tr>
-					<td height="500px" align="right">
-					</td> 
-					<td align="right" >
-						<div align="right" className="TopUp">  
-                             <p class="back-to-top" > <a href="#top" onClick="topFunction()"><i class="fa fa-2x fa-angle-up" id="faup"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </p>
-                        </div>
-					</td> 
-					 
-				</tr>
-				</table>
-				
-					<div className="burger">
-						<input className="menu-btn" type="checkbox" id="menu-btn" />
-						<label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
-						<ul className="menu">	
-						  	<li><Link to="/">Home</Link></li>
-             				<li><Link to="/Blog">Blog</Link></li>
-							<li><Link to="/Contact"></Link></li>
-						</ul>
-					</div>
-					
-			 	</div>
-	
-				);
-			}
-		}
+  render() {
+    return (
+      <div className="d-flex justify-content-between  align-items-center px-3">
+        <img
+          src={MA_Logo}
+          alt="site logo"
+          className="nav-logo align-items-center"
+        ></img>
+        <nav className="navbar navbar-expand-lg" role="navigation">
+          <Link className="nav-item nav-link" to="/">
+            Home
+          </Link>
+          <a className="nav-item nav-link" href="#Solutions">
+            Solutions
+          </a>
+          <a className="nav-item nav-link" href="#OurStory">
+            Our Story
+          </a>
+          <a className="nav-item nav-link" href="#Team">
+            Team
+          </a>
+          <a className="nav-item nav-link" href="#Portfolio">
+            Portfolio
+          </a>
+          <a className="nav-item nav-link" href="#Reviews">
+            Reviews
+          </a>
+          <a className="nav-item nav-link" href="#ContactUs">
+            Contact us
+          </a>
+        </nav>
 
-		window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("faup").style.display = "block";
-  } else {
-    document.getElementById("faup").style.display = "none";
+        <div className="burger">
+          <input className="menu-btn" type="checkbox" id="menu-btn" />
+          <label className="menu-icon" htmlFor="menu-btn">
+            <span className="navicon"></span>
+          </label>
+          <ul className="menu">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Blog">Blog</Link>
+            </li>
+            <li>
+              <Link to="/Contact"></Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
   }
-}
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
 }
 
 export default HeaderNav;
