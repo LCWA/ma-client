401beaabbe5c0763325b8cc27f6f2ec917aa424f
fadingbeat <fadingbeat@gmail.com>
2019-10-21 09:21:53 +0200

    Our Story Component

--- src/components/Spinning.js
+++ src/components/Spinning.js
@@ -1,64 +1,62 @@
-import React, { Component } from 'react';
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
 import * as THREE from "three";
-import OrbitControls from 'orbit-controls-es6';
+import OrbitControls from "orbit-controls-es6";
 
 class Spinning extends Component {
-    componentDidMount() {
+  componentDidMount() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
 
-                var scene = new THREE.Scene();
-                var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+    this.mount.appendChild(renderer.domElement);
+    window.addEventListener("resize", function() {
+      var width = window.innerHeight;
+      var height = window.innerHeight;
 
-                var renderer = new THREE.WebGLRenderer();
-                renderer.setSize(window.innerWidth, window.innerHeight);
-                //document.body.appendChild(renderer.domElement);
-                this.mount.appendChild( renderer.domElement );
-                window.addEventListener('resize', function(){
+      renderer.setSize(width / 2, height / 2);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+    });
+    this.controls = new OrbitControls(camera, renderer.domElement);
+    var geometry = new THREE.SphereGeometry(1, 8, 8);
+    var material = new THREE.MeshBasicMaterial({
+      color: 0xff6602,
+      wireframe: true
+    });
+    var sphere = new THREE.Mesh(geometry, material);
+    scene.add(sphere);
 
-                    var width = window.innerHeight;
-                    var height = window.innerHeight;
+    camera.position.z = 1;
 
-                    renderer.setSize(width, height);
-                    camera.aspect = width/height;
-                    camera.updateProjectionMatrix();
-                });
+    var update = function() {
+      sphere.rotation.x += 0.01;
+      sphere.rotation.y += 0.0005;
+    };
 
-                //var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    var render = function() {
+      renderer.render(scene, camera);
+    };
 
-                //const controls = new OrbitControls(camera, renderer.domElement);
-                this.controls = new OrbitControls(camera, renderer.domElement);
-                var geometry = new THREE.SphereGeometry( 1, 8, 8 );
-                var material = new THREE.MeshBasicMaterial( {color: 0x00ff00, wireframe: true} );
-                var sphere = new THREE.Mesh( geometry, material );
-                scene.add( sphere );
-                
-                camera.position.z = 1;
+    var GameLoop = function() {
+      requestAnimationFrame(GameLoop);
 
-                var update = function(){
-                    sphere.rotation.x += 0.01;
-                    sphere.rotation.y += 0.0005;
-                };
+      update();
+      render();
+    };
 
-                var render = function(){
-                    renderer.render(scene, camera);
-                }
-
-                var GameLoop = function(){
-                    requestAnimationFrame(GameLoop);
-
-                    update();
-                    render();
-                };
-
-                GameLoop();
-            }
-            render() {
-                return (
-                  <div ref={ref => (this.mount = ref)} />
-                )
-            }
-        
+    GameLoop();
+  }
+  render() {
+    return (
+      <div className="spinning-animation" ref={ref => (this.mount = ref)} />
+    );
+  }
 }
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Spinning/>, rootElement);
+
 export default Spinning;
