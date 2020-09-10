88da346cc29efa11dedac7c287805650f76f3297
beat <fadingbeat@gmail.com>
2020-09-10 13:58:06 +0200

    146-ma-website-updates-id146

--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { Component } from "react";
 import websites from "../images/solutions/websites.PNG";
-import automation_tools from "../images/solutions/automation_tools.PNG";
+import automation_tools from "../images/solutions/AI_ML.jpg";
 import customized_software from "../images/solutions/customized_software.PNG";
 import "../styles/Solutions.css";
 import { Link } from "react-scroll";
@@ -60,9 +60,94 @@ class Solutions extends Component {
   }
   render() {
     return (
-      <div className="solutions-wrapper">
+      <div className="solutions-wrapper" id="Solutions">
         <h1 className="solutions-title">Solutions We Offer</h1>
         <div className="card-wrapper">
+          <div className="solution customized-software">
+            <img
+              src={customized_software}
+              className="solution-image"
+              alt="Picture of customized software"
+            ></img>
+            <h2 className="title-customized">Custom Solutions</h2>
+            <p className="body-text description">
+              You have a specific idea of a digital solution that your company
+              needs? You know that there are software tools that could help your
+              business grow?
+              <br></br>
+              <span id="dots-three">...</span>
+              <span id="more-text-three">
+                <span id="top-description">
+                  <br></br>
+                </span>{" "}
+                <br></br>
+                At <span id="mission-automate">Mission Automate</span> we
+                architect and craft custom solutions that are tailored to your
+                specifications. The result is easy to use, bespoke software
+                built to address your needs and to create value in your company.{" "}
+                <span className="contact-notice">
+                  If you are ready to make your company more digital{" "}
+                  <a href="#ContactUs" id="inner-contact">
+                    Contact Us
+                  </a>{" "}
+                  and book your free discovery call with us.
+                </span>
+              </span>
+            </p>
+            <Link
+              className="btn btn-primary my-2 my-sm-0"
+              id="btn-three"
+              onClick={this.readMoreThree}
+              activeClass="active"
+              to="more-text-three"
+              spy={true}
+              smooth={true}
+              offset={-300}
+              duration={500}
+            >
+              Read More
+            </Link>
+          </div>
+          <div className="solution automation-tools">
+            <img
+              src={automation_tools}
+              className="solution-image"
+              alt="Picture of automation tools"
+            ></img>
+            <h2 className="title-automation">AI/ML Development</h2>
+            <p className="body-text description">
+              Machine Learning helps companies make significant progress. We use
+              the latest ML technologies to help you make sense of your current
+              data and get insight into the future.
+              <br></br>
+              <span id="dots-two">...</span>
+              <span id="more-text-two">
+                <br></br>
+                We develop both Cloud and On-Premise solutions based on what’s
+                most useful for you.
+                <span className="contact-notice">
+                  If you are ready for the upgrade{" "}
+                  <a href="#ContactUs">
+                    <span id="site-audit">contact us</span>
+                  </a>
+                  .
+                </span>
+              </span>
+            </p>
+            <Link
+              className="btn btn-primary my-2 my-sm-0"
+              id="btn-two"
+              onClick={this.readMoreTwo}
+              activeClass="active"
+              to="more-text-two"
+              spy={true}
+              smooth={true}
+              offset={-300}
+              duration={500}
+            >
+              Read More
+            </Link>
+          </div>
           <div className="solution websites">
             <img
               src={websites}
@@ -71,8 +156,10 @@ class Solutions extends Component {
             ></img>
             <h2 className="title-websites">Web Development</h2>
             <p className="body-text description">
-              A website is a necessity and no longer an option in order to make
-              your business excel.
+              In a modern world, having a website is no longer just an option,
+              it is more of a necessity if you want to make your business excel
+              and thrive.
+              <br></br>
               <span id="dots">...</span>
               <span id="more-text-one">
                 <br></br>
@@ -96,91 +183,6 @@ class Solutions extends Component {
             >
               Read More
             </button>
-          </div>
-          <div className="solution automation-tools">
-            <img
-              src={automation_tools}
-              className="solution-image"
-              alt="Picture of automation tools"
-            ></img>
-            <h2 className="title-automation">
-              SEO &amp; Social Media Management
-            </h2>
-            <p className="body-text description">
-              Reach new customers! Improve your website search rank and expand
-              with Google/Facebook/Instagram ads.
-              <span id="dots-two">...</span>
-              <span id="more-text-two">
-                <br></br>
-                You will get assistance with creating and managing ad campaigns
-                as well as an understanding of your important keywords and SEO
-                strategies.
-                <span className="contact-notice">
-                  If you want to get a{" "}
-                  <a href="#ContactUs">
-                    <span id="site-audit">Free Site Audit</span>
-                  </a>{" "}
-                  contact us and book a free discovery call with us.
-                </span>
-              </span>
-            </p>
-            <Link
-              className="btn btn-primary my-2 my-sm-0"
-              id="btn-two"
-              onClick={this.readMoreTwo}
-              activeClass="active"
-              to="more-text-two"
-              spy={true}
-              smooth={true}
-              offset={-300}
-              duration={500}
-            >
-              Read More
-            </Link>
-          </div>
-          <div className="solution customized-software">
-            <img
-              src={customized_software}
-              className="solution-image"
-              alt="Picture of customized software"
-            ></img>
-            <h2 className="title-customized">Custom Solutions</h2>
-            <p className="body-text description">
-              You have a specific idea of a digital solution that your company
-              needs? <span id="dots-three">...</span>
-              <span id="more-text-three">
-                <span id="top-description">
-                  <br></br>
-                  You know that there are software tools that could help your
-                  business grow?
-                </span>{" "}
-                <br></br>
-                At <span id="mission-automate">Mission Automate</span> we
-                architect and craft custom solutions that are tailored to your
-                specifications. The result is easy to use, bespoke software
-                built to address your needs and to create value in your company.{" "}
-                <span className="contact-notice">
-                  If you are ready for making your company more digital{" "}
-                  <a href="#ContactUs" id="inner-contact">
-                    Contact Us
-                  </a>{" "}
-                  and book your free discovery call with us.
-                </span>
-              </span>
-            </p>
-            <Link
-              className="btn btn-primary my-2 my-sm-0"
-              id="btn-three"
-              onClick={this.readMoreThree}
-              activeClass="active"
-              to="more-text-three"
-              spy={true}
-              smooth={true}
-              offset={-300}
-              duration={500}
-            >
-              Read More
-            </Link>
           </div>
         </div>
       </div>
